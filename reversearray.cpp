aa7554d11d5d11ff26c9936cbf549be59da192a4
albertsingh <albert531singh@gmail.com>
2025-03-26 01:30:13 +0530

    Simplify reverse function by using std::swap for element swapping

--- reversearray.cpp
+++ reversearray.cpp
@@ -6,9 +6,7 @@ void reverse(int arr[], int n){
     int end=n-1;
     
     while(start<=end){
-    int temp=arr[start];
-    arr[start]=arr[end];
-    arr[end]=temp;
+    swap(arr[start],arr[end]);
     start++;
     end--;
     }
