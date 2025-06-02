8df858e202fa5d7347803a76b963ede4a7bede7b
albertsingh <albert531singh@gmail.com>
2025-06-02 21:44:13 +0530

    candyproblem

--- candy135.cpp
+++ candy135.cpp
@@ -2,27 +2,27 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-    int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> candies(n, 1); 
-        for(int i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1]) {
-                candies[i] = candies[i-1] + 1;
-            }
+int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    vector<int> candies(n, 1); 
+    for(int i=1; i<n; i++){
+        if(ratings[i] > ratings[i-1]) {
+            candies[i] = candies[i-1] + 1;
         }
-        for(int i=n-2; i>=0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                candies[i] = max(candies[i], candies[i+1] + 1);
-            }
-        }
-        int totalCandies = 0;
-        for(auto &c : candies) {
-            cout << c << " "; // Print the candies distribution for debugging
-            totalCandies += c;
-        }
-        return totalCandies;
-
     }
+    for(int i=n-2; i>=0; i--) {
+        if(ratings[i] > ratings[i+1]) {
+            candies[i] = max(candies[i], candies[i+1] + 1);
+        }
+    }
+    int totalCandies = 0;
+    for(auto &c : candies) {
+        cout << c << " "; // Print the candies distribution for debugging
+        totalCandies += c;
+    }
+    return totalCandies;
+
+}
 
 
 
