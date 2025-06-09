3d71d18a113c42736b4c33f544c1863be624eaa4
albertsingh <albert531singh@gmail.com>
2025-06-09 17:02:10 +0530

    update

--- lexicographicallynumber.cpp
+++ lexicographicallynumber.cpp
@@ -18,7 +18,35 @@ vector<int> lexicalOrder(int n) {  // time complexity -> 0(n^2)
         
 }
 
-void dfs(int temp , int n , vector<int>&sol){
+int findKthNumber1(int n, int k) {  // time complexity -> 0(n^2)
+    vector<string>st;
+    for(int i=1 ; i<=n; i++){
+        st.push_back(to_string(i)); // change the int to string value -> using *to_string* function 
+    }
+    vector<int>result;
+    sort(st.begin(), st.end()); // sort the string element of st in lexicographically order 
+    for(auto it : st){
+        result.push_back(stoi(it)); // change the string to int value -> using *stoi* function 
+    }
+    return result[k-1];
+        
+}
+
+void dfs(int temp , int n , vector<int>&sol , int k){
+    if(temp>n){
+        return ;
+    }
+    sol.push_back(temp);
+    for(int i=0; i<=k; i++){
+        int next = temp*10 + i;
+        if(next>n){
+            break;
+        }
+        dfs(next , n , sol , k);
+    }
+}
+
+void dfs1(int temp , int n , vector<int>&sol){
     if(temp>n){
         return ;
     }
@@ -28,16 +56,27 @@ void dfs(int temp , int n , vector<int>&sol){
         if(next>n){
             break;
         }
-        dfs(next , n , sol);
+        dfs1(next , n , sol);
     }
 }
 
 vector<int> lexicalOrder1(int n) {
     vector<int>sol;
     for(int i=1; i<=9; i++){
-        dfs(i, n , sol);
+        dfs1(i, n , sol);
     }
     return sol;    
+}
+
+int findKthNumber(int n, int k) {
+    
+    vector<int>sol;
+    for(int i=1; i<=9; i++){
+        dfs(i, n , sol , k);
+    }
+
+    return sol[k-1];
+
 }
 
 
@@ -50,6 +89,10 @@ int main(){
     for(auto i : st1){
         cout<<i<<" ";
     }
+    cout<<endl;
+
+    cout<<findKthNumber(n , 2)<<endl;
+    //cout<<findKthNumber1(n,2)<<endl;
     return 0;
 
 }
