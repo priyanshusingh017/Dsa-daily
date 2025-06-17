61c52966e740b34a7cb7f3e034b0f5abb0902c06
albertsingh <albert531singh@gmail.com>
2025-06-17 23:30:05 +0530

    maxdistance-leetcode2078

---
+++ leetcode/leetcode2078.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxDistance(vector<int>& colors) {
+    int maxdis=0;
+    int n = colors.size();
+    // check the first house 
+    for(int i=n-1; i>0; i--){
+        if(colors[0] != colors[i]){
+            maxdis = max(maxdis , i);
+            break; // Found a different color, no need to check further
+        }
+    }
+    // check the last house
+    for(int i=0; i<n-1; i++){
+        if(colors[n-1] != colors[i]){
+            maxdis = max(maxdis , n-1-i);
+            break; // Found a different color, no need to check further
+        }
+    }
+    return maxdis;
+}
+
+
+int main(){
+    vector<int> nums = {1,1,1,6,1,1,1};
+    cout << maxDistance(nums) << endl; // Output: 4-> (5-1)
+    return 0; 
+}
