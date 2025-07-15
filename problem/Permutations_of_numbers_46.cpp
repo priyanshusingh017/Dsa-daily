18f4d312fb662e611e06366656bfd69a027e3ad2
albertsingh <albert531singh@gmail.com>
2025-07-15 10:51:35 +0530

    permutation_of_number_leetcode_46.cpp

--- problem/Permutations_of_numbers_46.cpp
+++ problem/Permutations_of_numbers_46.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 #include<vector>
 using namespace std;
 
@@ -22,7 +23,7 @@ void solve(vector<int>nums , vector<vector<int>> & ans , int index){
 
 }
 vector<vector<int>> permute(vector<int>& nums){
-    vector<vector<int>>ans;
+    vector<vector<int>>ans; 
     int index =0;
     solve(nums , ans , index);
     return ans;
@@ -31,7 +32,7 @@ vector<vector<int>> permute(vector<int>& nums){
 
 int main(){
 
-    vector<int>nums = {1 , 2 ,3};  // 3! = 6 number 
+    vector<int>nums = {1,1,5};  // 3! = 6 number 
     vector<vector<int>>res = permute(nums);
     for(auto i: res){
         cout<<"[";
