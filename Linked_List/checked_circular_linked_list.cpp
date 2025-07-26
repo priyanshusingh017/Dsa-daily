c6a2ebaae417cbcc41f2cd77b515aa1b7eea3650
albertsingh <albert531singh@gmail.com>
2025-07-27 01:49:13 +0530

    checked_circular_list.cpp

---
+++ Linked_List/checked_circular_linked_list.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+void insertNode(Node *&tail, int element, int d)
+{
+
+    // empty list
+    if (tail == nullptr)
+    {
+        Node *temp = new Node(d);
+        tail = temp;
+        temp->next = temp;
+    }
+    else
+    {
+
+        Node *curr = tail;
+        while (curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        Node *temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+// traversal
+void print(Node *&tail)
+{
+
+    if (tail == nullptr)
+    {
+        cout << "empty" << endl;
+        return;
+    }
+
+    Node *temp = tail;
+    do
+    {
+        cout << tail->data << " ";
+        tail = tail->next;
+    } while (tail != temp);
+    cout << endl;
+}
+
+bool iscircularList(Node*head){
+
+    //basecase
+    if(head) return true;
+
+    Node*temp = head->next;
+
+    while(temp!=nullptr && temp !=head){
+        temp = temp->next;
+    }
+
+    if(temp==nullptr){
+        return false;
+    }
+    if(temp ==head){
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+
+    Node *tail = nullptr;
+
+    insertNode(tail, 2, 10);
+    print(tail);
+
+    insertNode(tail, 10, 20);
+    print(tail);
+
+    insertNode(tail, 20, 890);
+    print(tail);
+
+    insertNode(tail, 20, 130);
+    print(tail);
+
+
+    if(iscircularList(tail)){
+        cout<<"Y"<<endl;
+    }
+    else{
+        cout<<"N"<<endl;
+    }
+
+    return 0;
+
+    //floyd's cycle detection algorithm
+}
