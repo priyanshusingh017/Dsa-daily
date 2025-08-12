582f05965c68f4ea6a0ebda39258f5a644e77cd3
albertsingh <albert531singh@gmail.com>
2025-08-12 22:55:03 +0530

    sliding_window_maximum.cpp

--- Queues/sliding_window_max_value.cpp
+++ Queues/sliding_window_max_value.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+// approach 1 -> 
 vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     int n = nums.size();
     deque<int>q;
@@ -26,11 +28,53 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     return ans;
 }
 
+// approach 2 -> 
+vector<int>maxslidingwindow(vector<int>&nums , int k){
+   
+    int n = nums.size(); // size of the array 
+
+    deque<int>q; 
+    vector<int>ans;
+
+    // traversal and perform the operation from the 0 to k element or first sliding window 
+    for(int i=0; i<k; i++){
+        // tackle with smaller elemnent 
+        while(!q.empty() && nums[q.back()]<nums[i]){
+            q.pop_back();
+        }
+        // adding the index in the deque 
+        q.push_back(i);
+
+        //not add the num to answer until reach the kth element 
+        if(i>=k-1){
+            ans.push_back(nums[q.front()]);
+        }
+    }
+
+    // traversal and perform the operation from k to n or next sliding window
+    for(int i=k; i<n; i++){
+        // check the sliding window condition 
+        if(!q.empty() && i-q.front()>=k){
+            q.pop_front();
+        }
+        // remove the smaller element index comparison to the current element 
+        while(!q.empty() && nums[q.back()]<nums[i]){
+            q.pop_back();
+        }
+        // adding the index to the deque
+        q.push_back(i);
+        // now the sliding window maintain with moving to the next element -> so no need to add the condition here 
+        ans.push_back(nums[q.front()]);
+    }
+
+    return ans;
+}
+
 int main(){
 
     vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
     int k = 3;
-    vector<int> result = maxSlidingWindow(nums, k);
+    vector<int> result = maxslidingwindow(nums, k);
     cout << "Sliding window max values: ";
     for (int x : result) cout << x << " ";
     cout << endl;
