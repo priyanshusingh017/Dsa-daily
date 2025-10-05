f2067261b09bc253622f4dfd61abe674315c7328
albertsingh <albert531singh@gmail.com>
2025-10-05 19:42:06 +0530

    update

--- striver/Array/RotateArray.cpp
+++ striver/Array/RotateArray.cpp
@@ -11,13 +11,35 @@ void rotate(vector<int>& nums, int k) {
     nums=temp;
 }
 
-int main(){
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+void rotateright(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int>temp1(n);
+    for(int i=0; i<nums.size(); i++){
+        temp1[i] = nums[(i+k)%n];
+    }
+    nums=temp1;
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 4, 5, 6, 7};
+    vector<int> nums2 = {1, 2, 3, 4, 5, 6, 7};
     int k = 3;
-    rotate(nums, k);
-    for(int num : nums) {
+
+    // Using rotate (left rotation)
+    rotate(nums1, k);
+    cout << "After rotate (left): ";
+    for (int num : nums1) {
         cout << num << " ";
     }
     cout << endl;
+
+    // Using rotateright (right rotation)
+    rotateright(nums2, k);
+    cout << "After rotateright (right): ";
+    for (int num : nums2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
